326728df1e46493baf62742bdecdcd5d33e58268
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-11-21 10:13:49 -0300

    codigo reorganizado parcialmente em classes

--- main/main.ts
+++ main/main.ts
@@ -1,13 +1,14 @@
-import { iniciarJogo, iniciarCombate, ask } from "../service/BattleManager/GameLoop";
+import { GameLoop } from "../service/BattleManager/GameLoop";
 
 (async () => {
-    const nome = await ask("Escolha seu Nome de Jogador: ");
+    const jogo = new GameLoop();
+    const nome = await jogo.ask("Escolha seu Nome de Jogador: ");
     console.log("[1] Guerreiro");
     console.log("[2] Mago");
 
-    const escolhaStr = await ask("Escolha sua Classe: ");
+    const escolhaStr = await jogo.ask("Escolha sua Classe: ");
     const escolha = Number(escolhaStr);
 
-    iniciarJogo(nome, escolha);
-    iniciarCombate();
+    jogo.iniciarJogo(nome, escolha);
+    jogo.iniciarCombate();
 })();
--- service/BattleManager/GameLoop.ts
+++ service/BattleManager/GameLoop.ts
@@ -1,118 +1,103 @@
 import readline from "readline";
 import { atacar, ataqueMagico, curar } from "../CombatService/Combat";
 import { Personagem } from "../../model/Personagem";
-import { Guerreiro } from "../../model";
-import { Mago } from "../../model";
+import { Guerreiro } from "../../model/Guerreiro";
+import { Mago } from "../../model/Mago";
 import { GameUI } from "../../view/TerminalView";
 
-let principal: Personagem;
-let alvo: Personagem;
-
-// ========================
-//  Sistema de Input
-// ========================
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-export function ask(question: string): Promise<string> {
-    return new Promise(resolve => {
-        rl.question(question, resolve);
+export class GameLoop {
+    private principal!: Personagem;
+    private alvo!: Personagem;
+    private rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
-}
 
-// ========================
-//  Inicialização do Jogo
-// ========================
-export function iniciarJogo(nome: string, escolhaClasse: number): Personagem {
-    switch (escolhaClasse) {
-        case 1:
-            alvo = new Mago("Merlin");
-            return principal = new Guerreiro(nome);
-
-        case 2:
-            alvo = new Guerreiro("Arthur");
-            return principal = new Mago(nome);
-
-        default:
-            throw new Error("Classe inválida.");
-    }
-}
-
-// ========================
-//  Turno do Jogador
-// ========================
-function turnoJogador(escolha: number) {
-    switch (escolha) {
-        case 1:
-            return atacar(principal, alvo);
-
-        case 2:
-            return curar(principal);
-
-        case 3:
-            return ataqueMagico(principal, alvo);
-
-        default:
-            console.log("Escolha inválida!");
-    }
-}
-
-// ========================
-//  Turno do Inimigo (IA)
-// ========================
-function turnoInimigo() {
-    const chance = Math.random();
-
-    if (
-        alvo.gethp() <= 0.15 * alvo.gethpMax() && 
-        alvo.getQtdPot() > 0 && 
-        chance < 0.3
-    ) {
-        let cura = curar(alvo);
-        GameUI.mensagem("\nO inimigo usa uma poção e curou " + cura);
-        return cura;
-    } else if(alvo.getMana() > 5 && chance < 0.7){
-        let dano = ataqueMagico(alvo, principal);
-        GameUI.mensagem("\nO inimigo usou um Ataque Magico e causou " + dano + " de dano magico!");
-        return dano;
+    public ask(question: string): Promise<string> {
+        return new Promise(resolve => {
+            this.rl.question(question, resolve);
+        });
     }
 
-    GameUI.mensagem("\nO inimigo te ataca com um Ataque Fisico!");
-    return atacar(alvo, principal);
-}
+    public iniciarJogo(nome: string, escolhaClasse: number): Personagem {
+        switch (escolhaClasse) {
+            case 1:
+                this.alvo = new Mago("Merlin");
+                this.principal = new Guerreiro(nome);
+                return this.principal;
 
-// ========================
-//  Loop de Combate
-// ========================
-export async function iniciarCombate() {
+            case 2:
+                this.alvo = new Guerreiro("Arthur");
+                this.principal = new Mago(nome);
+                return this.principal;
 
-    GameUI.mensagem("\n=== COMBATE INICIADO ===");
-
-    while (alvo.gethp() > 0 && principal.gethp() > 0) {
-
-        GameUI.exibirStatus(principal, alvo);
-        GameUI.exibirAcoes();
-
-        const entrada = await ask("Escolha sua ação: ");
-        const escolha = Number(entrada);
-
-        turnoJogador(escolha);
-
-        if (alvo.gethp() <= 0) {
-            GameUI.mensagem("\n🎉 Você venceu o combate!");
-            break;
-        }
-
-        turnoInimigo();
-
-        if (principal.gethp() <= 0) {
-            GameUI.mensagem("\n💀 Você foi derrotado...");
-            break;
+            default:
+                throw new Error("Classe inválida.");
         }
     }
-    rl.close();
+
+    private turnoJogador(escolha: number) {
+        switch (escolha) {
+            case 1:
+                return atacar(this.principal, this.alvo);
+
+            case 2:
+                return curar(this.principal);
+
+            case 3:
+                return ataqueMagico(this.principal, this.alvo);
+
+            default:
+                console.log("Escolha inválida!");
+        }
+    }
+
+    private turnoInimigo() {
+        const chance = Math.random();
+
+        if (
+            this.alvo.gethp() <= 0.15 * this.alvo.gethpMax() &&
+            this.alvo.getQtdPot() > 0 &&
+            chance < 0.3
+        ) {
+            let cura = curar(this.alvo);
+            GameUI.mensagem("\nO inimigo usa uma poção e curou " + cura);
+            return cura;
+        } else if (this.alvo.getMana() > 5 && chance < 0.7) {
+            let dano = ataqueMagico(this.alvo, this.principal);
+            GameUI.mensagem("\nO inimigo usou um Ataque Magico e causou " + dano + " de dano magico!");
+            return dano;
+        }
+
+        GameUI.mensagem("\nO inimigo te ataca com um Ataque Fisico!");
+        return atacar(this.alvo, this.principal);
+    }
+
+    public async iniciarCombate() {
+        GameUI.mensagem("\n=== COMBATE INICIADO ===");
+
+        while (this.alvo.gethp() > 0 && this.principal.gethp() > 0) {
+
+            GameUI.exibirStatus(this.principal, this.alvo);
+            GameUI.exibirAcoes();
+
+            const entrada = await this.ask("Escolha sua ação: ");
+            const escolha = Number(entrada);
+
+            this.turnoJogador(escolha);
+
+            if (this.alvo.gethp() <= 0) {
+                GameUI.mensagem("\n🎉 Você venceu o combate!");
+                break;
+            }
+
+            this.turnoInimigo();
+
+            if (this.principal.gethp() <= 0) {
+                GameUI.mensagem("\n💀 Você foi derrotado...");
+                break;
+            }
+        }
+        this.rl.close();
+    }
 }
-
-
